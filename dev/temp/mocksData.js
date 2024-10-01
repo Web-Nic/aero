c012d4a0781852010f2df791ad6cc632df9466d0
Web-Nic <1000.alexnik@gmail.com>
2024-10-01 19:42:00 +0300

    start

---
+++ dev/temp/mocksData.js
@@ -0,0 +1,257 @@
+'head': {
+    defaults: {
+        title: 'Index',
+        indexPage: true
+    },
+    team: {
+        title: 'Team',
+        indexPage: false
+    },
+    home: {
+        title: 'Home',
+        indexPage: false
+    }
+}
+,
+
+__iconsData: {
+    
+        'accessibility': {
+            width: '20px',
+            height: '20px'
+        },
+    
+        'arr-down': {
+            width: '10px',
+            height: '7px'
+        },
+    
+        'arr-right': {
+            width: '18px',
+            height: '12px'
+        },
+    
+        'burger': {
+            width: '20px',
+            height: '20px'
+        },
+    
+        'calendar': {
+            width: '32px',
+            height: '32px'
+        },
+    
+        'case': {
+            width: '60px',
+            height: '60px'
+        },
+    
+        'click': {
+            width: '60px',
+            height: '60px'
+        },
+    
+        'close': {
+            width: '20px',
+            height: '20px'
+        },
+    
+        'date': {
+            width: '18px',
+            height: '18px'
+        },
+    
+        'disabled-sign': {
+            width: '60px',
+            height: '60px'
+        },
+    
+        'doc': {
+            width: '40px',
+            height: '40px'
+        },
+    
+        'download': {
+            width: '16px',
+            height: '18px'
+        },
+    
+        'earth': {
+            width: '40px',
+            height: '40px'
+        },
+    
+        'filter': {
+            width: '18px',
+            height: '18px'
+        },
+    
+        'flag': {
+            width: '18px',
+            height: '18px'
+        },
+    
+        'globe': {
+            width: '28px',
+            height: '28px'
+        },
+    
+        'map-2': {
+            width: '40px',
+            height: '40px'
+        },
+    
+        'map': {
+            width: '47px',
+            height: '47px'
+        },
+    
+        'package': {
+            width: '55.24px',
+            height: '60px'
+        },
+    
+        'parking-2': {
+            width: '40px',
+            height: '40px'
+        },
+    
+        'parking': {
+            width: '48.05px',
+            height: '60px'
+        },
+    
+        'plus': {
+            width: '20px',
+            height: '20px'
+        },
+    
+        'search-2': {
+            width: '15px',
+            height: '15px'
+        },
+    
+        'search': {
+            width: '24px',
+            height: '24px'
+        },
+    
+        'soc-fb': {
+            width: '9px',
+            height: '16px'
+        },
+    
+        'soc-inst': {
+            width: '16px',
+            height: '16px'
+        },
+    
+        'soc-od': {
+            width: '10px',
+            height: '16px'
+        },
+    
+        'soc-tw': {
+            width: '16px',
+            height: '14px'
+        },
+    
+        'soc-vk': {
+            width: '16px',
+            height: '9px'
+        },
+    
+        'soc-yt': {
+            width: '16px',
+            height: '11px'
+        },
+    
+        'spy': {
+            width: '56.21px',
+            height: '60px'
+        },
+    
+        'star': {
+            width: '60px',
+            height: '57.24px'
+        },
+    
+        'tickets-2': {
+            width: '40px',
+            height: '40px'
+        },
+    
+        'tickets': {
+            width: '60px',
+            height: '60px'
+        },
+    
+},
+
+__pages: [{
+                name: 'about',
+                href: 'about.html'
+             },{
+                name: 'all',
+                href: 'all.html'
+             },{
+                name: 'card-sez',
+                href: 'card-sez.html'
+             },{
+                name: 'contacts',
+                href: 'contacts.html'
+             },{
+                name: 'feedback',
+                href: 'feedback.html'
+             },{
+                name: 'flight-cancel',
+                href: 'flight-cancel.html'
+             },{
+                name: 'flight',
+                href: 'flight.html'
+             },{
+                name: 'index',
+                href: 'index.html'
+             },{
+                name: 'news-item-anim',
+                href: 'news-item-anim.html'
+             },{
+                name: 'news-item',
+                href: 'news-item.html'
+             },{
+                name: 'news',
+                href: 'news.html'
+             },{
+                name: 'page',
+                href: 'page.html'
+             },{
+                name: 'regulations',
+                href: 'regulations.html'
+             },{
+                name: 'schedule',
+                href: 'schedule.html'
+             },{
+                name: 'scoreboard',
+                href: 'scoreboard.html'
+             },{
+                name: 'scoreboard_arrival',
+                href: 'scoreboard_arrival.html'
+             },{
+                name: 'scoreboard_transfer',
+                href: 'scoreboard_transfer.html'
+             },{
+                name: 'search-full',
+                href: 'search-full.html'
+             },{
+                name: 'search',
+                href: 'search.html'
+             },{
+                name: 'services-item',
+                href: 'services-item.html'
+             },{
+                name: 'services',
+                href: 'services.html'
+             },{
+                name: 'ui',
+                href: 'ui.html'
+             }]
---
+++ dev/temp/symbols-data-template.js
@@ -0,0 +1,173 @@
+__iconsData: {
+    
+        'accessibility': {
+            width: '20px',
+            height: '20px'
+        },
+    
+        'arr-down': {
+            width: '10px',
+            height: '7px'
+        },
+    
+        'arr-right': {
+            width: '18px',
+            height: '12px'
+        },
+    
+        'burger': {
+            width: '20px',
+            height: '20px'
+        },
+    
+        'calendar': {
+            width: '32px',
+            height: '32px'
+        },
+    
+        'case': {
+            width: '60px',
+            height: '60px'
+        },
+    
+        'click': {
+            width: '60px',
+            height: '60px'
+        },
+    
+        'close': {
+            width: '20px',
+            height: '20px'
+        },
+    
+        'date': {
+            width: '18px',
+            height: '18px'
+        },
+    
+        'disabled-sign': {
+            width: '60px',
+            height: '60px'
+        },
+    
+        'doc': {
+            width: '40px',
+            height: '40px'
+        },
+    
+        'download': {
+            width: '16px',
+            height: '18px'
+        },
+    
+        'earth': {
+            width: '40px',
+            height: '40px'
+        },
+    
+        'filter': {
+            width: '18px',
+            height: '18px'
+        },
+    
+        'flag': {
+            width: '18px',
+            height: '18px'
+        },
+    
+        'globe': {
+            width: '28px',
+            height: '28px'
+        },
+    
+        'map-2': {
+            width: '40px',
+            height: '40px'
+        },
+    
+        'map': {
+            width: '47px',
+            height: '47px'
+        },
+    
+        'package': {
+            width: '55.24px',
+            height: '60px'
+        },
+    
+        'parking-2': {
+            width: '40px',
+            height: '40px'
+        },
+    
+        'parking': {
+            width: '48.05px',
+            height: '60px'
+        },
+    
+        'plus': {
+            width: '20px',
+            height: '20px'
+        },
+    
+        'search-2': {
+            width: '15px',
+            height: '15px'
+        },
+    
+        'search': {
+            width: '24px',
+            height: '24px'
+        },
+    
+        'soc-fb': {
+            width: '9px',
+            height: '16px'
+        },
+    
+        'soc-inst': {
+            width: '16px',
+            height: '16px'
+        },
+    
+        'soc-od': {
+            width: '10px',
+            height: '16px'
+        },
+    
+        'soc-tw': {
+            width: '16px',
+            height: '14px'
+        },
+    
+        'soc-vk': {
+            width: '16px',
+            height: '9px'
+        },
+    
+        'soc-yt': {
+            width: '16px',
+            height: '11px'
+        },
+    
+        'spy': {
+            width: '56.21px',
+            height: '60px'
+        },
+    
+        'star': {
+            width: '60px',
+            height: '57.24px'
+        },
+    
+        'tickets-2': {
+            width: '40px',
+            height: '40px'
+        },
+    
+        'tickets': {
+            width: '60px',
+            height: '60px'
+        },
+    
+}
